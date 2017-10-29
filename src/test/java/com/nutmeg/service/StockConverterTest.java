ff28ca69766dee95f5effae46ef7664ba1530e49
jan <jan.deulofeu@scalarmonitoring.com>
2017-10-29 21:29:12 +0000

    -Cleaning up

--- src/test/java/com/nutmeg/service/StockConverterTest.java
+++ src/test/java/com/nutmeg/service/StockConverterTest.java
@@ -3,10 +3,13 @@ package com.nutmeg.service;
 import com.nutmeg.model.Holding;
 import com.nutmeg.model.Stock;
 import com.nutmeg.repository.HoldingRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,7 +29,7 @@ public class StockConverterTest {
             "'NEAA0000,20170102,BOT,30,1.500,VUSA', 12.54 , 30",
             "'NEAA0000,20170201,DIV,0.2024,1,VUKE', 12.7424, 20",
             "'NEAA0000,20170201,SLD,20,2.000,VUSA', 52.7424, 10",
-            "'NEAA0000,20170201,BOT,10.512,3.3350,GILS', 17.6849, 10.512",
+            "'NEAA0000,20170201,BOT,10.512,3.3350,GILS', 17.6849, 10.512"
     })
     public void validateConverterCalculateCash(final String stockString, final String cash, final String holdings) {
 
@@ -43,4 +46,21 @@ public class StockConverterTest {
 
         assertThat(Double.valueOf(cash)).isEqualTo(holdingCash.getHolding());
     }
+
+
+    @Test
+    public void validateHoldingsZeroAreNotAddedToTheOutput() {
+        
+        HoldingRepository.reset();
+
+        final Stock stock = StockBuilder.buildStock("NEAA0000,20170102,BOT,0,0,VUKE");
+
+        new StockConverter().convert(Arrays.asList(stock));
+
+        final Map<String, List<Holding>> result = HoldingRepository.readAll();
+
+        assertThat(result).isEmpty();
+
+    }
+
 }
